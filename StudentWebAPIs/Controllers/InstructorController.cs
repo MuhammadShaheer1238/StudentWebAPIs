4bac654c552321fd7542c0382386e8da383801c4
Shaheer Butt <muhammadshaheer1238@gmail.com>
2023-02-24 12:18:26 -0500

    Revert "test2"
    This reverts commit 6a81181d5d9ffc7265a51a81efb9b105ca1c4b14.

--- StudentWebAPIs/Controllers/InstructorController.cs
+++ StudentWebAPIs/Controllers/InstructorController.cs
@@ -9,11 +9,6 @@ namespace StudentWebAPIs.Controllers
     [ApiController]
     [Route("api/[controller]")]
 
-
-
-
-
-
     public class InstructorController : Controller
     {
         public readonly IInstructorRepository _instructorRepository;
